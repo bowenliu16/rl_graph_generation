1885fe7af15be7b971921d119d9b445e83c4a405
JiaxuanYou <youjiaxuan@gmail.com>
2018-04-12 23:24:55 +0800

    add tensorboard support again

--- rl-baselines/baselines/ppo1/run_molecule.py
+++ rl-baselines/baselines/ppo1/run_molecule.py
@@ -63,7 +63,7 @@ def atari_arg_parser():
     parser.add_argument('--env', help='environment ID',
                         default='BreakoutNoFrameskip-v4')
     parser.add_argument('--seed', help='RNG seed', type=int, default=0)
-    parser.add_argument('--num-timesteps', type=int, default=int(10e6))
+    parser.add_argument('--num-timesteps', type=int, default=int(10e7))
     parser.add_argument('--name', type=str, default='test')
     return parser
 
