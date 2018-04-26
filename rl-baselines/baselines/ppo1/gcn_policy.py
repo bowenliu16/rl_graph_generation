96423e161b5b9dd5c1d348f8d3d11ce4ef9d74f4
JiaxuanYou <youjiaxuan@gmail.com>
2018-04-25 20:33:30 -0700

    lower stop prob by 10

--- rl-baselines/baselines/ppo1/gcn_policy.py
+++ rl-baselines/baselines/ppo1/gcn_policy.py
@@ -133,7 +133,9 @@ class GCNPolicy(object):
 
         ### 2 predict stop
         self.logits_stop = tf.reduce_sum(tf.layers.dense(emb_node, 32, activation=tf.nn.relu, name='linear_stop1'),axis=1)
-        self.logits_stop = tf.layers.dense(self.logits_stop, 2, activation=None, name='linear_stop2')  # B*2
+        self.logits_stop_yes = tf.layers.dense(self.logits_stop, 1, activation=None, name='linear_stop2_1')  # B*1
+        self.logits_stop_no = tf.layers.dense(self.logits_stop, 1, activation=None, name='linear_stop2_2')  # B*1
+        self.logits_stop = tf.concat((self.logits_stop_yes,self.logits_stop_no/10),axis=1)
         pd_stop = CategoricalPdType(-1).pdfromflat(flat=self.logits_stop)
         ac_stop = pd_stop.sample()
 
