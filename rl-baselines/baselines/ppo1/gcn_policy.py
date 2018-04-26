e823b8f82b6d70c9762561ca9eca9c525a40ab76
JiaxuanYou <youjiaxuan@gmail.com>
2018-04-25 23:28:20 -0700

    add final gan function, now with true data

--- rl-baselines/baselines/ppo1/gcn_policy.py
+++ rl-baselines/baselines/ppo1/gcn_policy.py
@@ -132,11 +132,11 @@ class GCNPolicy(object):
         logits_first_mask = tf.sequence_mask(ob_len_first,maxlen=tf.shape(ob['node'])[2]) # mask valid entry -3 (rm isolated nodes)
 
         ### 2 predict stop
-        # self.logits_stop = tf.reduce_sum(tf.layers.dense(emb_node, 32, activation=tf.nn.relu, name='linear_stop1'),axis=1)
-        # self.logits_stop = tf.layers.dense(self.logits_stop, 2, activation=None, name='linear_stop2_1')  # B*2
+        self.logits_stop = tf.reduce_sum(tf.layers.dense(emb_node, 32, activation=tf.nn.relu, name='linear_stop1'),axis=1)
+        self.logits_stop = tf.layers.dense(self.logits_stop, 2, activation=None, name='linear_stop2_1')  # B*2
         # explicitly show node num
-        self.logits_stop = tf.concat((tf.reduce_mean(tf.layers.dense(emb_node, 32, activation=tf.nn.relu, name='linear_stop1'),axis=1),tf.reshape(ob_len_first/5,[-1,1])),axis=1)
-        self.logits_stop = tf.layers.dense(self.logits_stop, 2, activation=None, name='linear_stop2')  # B*2
+        # self.logits_stop = tf.concat((tf.reduce_mean(tf.layers.dense(emb_node, 32, activation=tf.nn.relu, name='linear_stop1'),axis=1),tf.reshape(ob_len_first/5,[-1,1])),axis=1)
+        # self.logits_stop = tf.layers.dense(self.logits_stop, 2, activation=None, name='linear_stop2')  # B*2
 
         pd_stop = CategoricalPdType(-1).pdfromflat(flat=self.logits_stop+tf.constant([[0,-3]],dtype=tf.float32))
         ac_stop = pd_stop.sample()
