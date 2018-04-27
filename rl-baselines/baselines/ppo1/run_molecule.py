2b7fb74b2276b619e45fd116d159020099eabc8c
JiaxuanYou <youjiaxuan@gmail.com>
2018-04-27 13:08:52 -0700

    cannot load ckpt, trying out

--- rl-baselines/baselines/ppo1/run_molecule.py
+++ rl-baselines/baselines/ppo1/run_molecule.py
@@ -87,6 +87,7 @@ def molecule_arg_parser():
     parser.add_argument('--save_every', type=int, default=500)
     parser.add_argument('--load', type=int, default=1)
     parser.add_argument('--load_step', type=int, default=2000)
+    # parser.add_argument('--load_step', type=int, default=0)
     parser.add_argument('--curriculum', type=int, default=0)
     parser.add_argument('--curriculum_num', type=int, default=6)
     parser.add_argument('--curriculum_step', type=int, default=100)
