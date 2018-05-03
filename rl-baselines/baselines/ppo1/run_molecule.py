c3d8e73b84a8254b1ebe4b5580a9e97606a7b59f
JiaxuanYou <youjiaxuan@gmail.com>
2018-05-02 23:47:37 -0700

    update args

--- rl-baselines/baselines/ppo1/run_molecule.py
+++ rl-baselines/baselines/ppo1/run_molecule.py
@@ -27,7 +27,7 @@ def train(args,seed,writer=None):
     set_global_seeds(workerseed)
     if args.env=='molecule':
         env = gym.make('molecule-v0')
-        env.init(data_type=args.dataset,logp_ratio=args.logp_ratio,qed_ratio=args.qed_ratio,sa_ratio=args.sa_ratio,reward_step_total=args.reward_step_total,is_normalize=args.normalize_adj) # remember call this after gym.make!!
+        env.init(data_type=args.dataset,logp_ratio=args.logp_ratio,qed_ratio=args.qed_ratio,sa_ratio=args.sa_ratio,reward_step_total=args.reward_step_total,is_normalize=args.normalize_adj,reward_type=args.reward_type,reward_target=args.reward_target) # remember call this after gym.make!!
     elif args.env=='graph':
         env = GraphEnv()
         env.init(reward_step_total=args.reward_step_total,is_normalize=args.normalize_adj,dataset=args.dataset) # remember call this after gym.make!!
